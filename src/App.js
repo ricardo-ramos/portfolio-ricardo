7f2a30af9415645bb4f2f2675e563a6f0a8421af
Ricardo Ramos <ricardoalmeidaramos@gmail.com>
2021-05-21 23:07:25 -0300

    testes

--- src/App.js
+++ src/App.js
@@ -4,25 +4,39 @@ import ReactFullpage from "@fullpage/react-fullpage";
 import "fullpage.js/vendors/scrolloverflow";
 import Hello from "./components/Hello/Hello";
 import About from "./components/About/About";
+import Header from "./components/Header/Header";
+import styled from "styled-components";
 
+const Testeee = styled.button`
+  position: fixed;
+  z-index: 1;
+  top: 4%;
+`
 const App = () => {
   return (
     <>
       <GlobalStyle />
+      {/* <Header /> */}
+      {/* <a href="#sectiontest"> testeeeee </a> */}
       <ReactFullpage
         scrollOverflow={true}
-        render={({ state, fullpageApi }) => {
+        licenseKey={null}
+        fixedElements=".fixedHeader"
+        render={({ fullpageApi }) => {
           return (
-            <ReactFullpage.Wrapper>
-              <div className="section">
-                <Hello />
-              </div>
-              <div className="section">
-                <About />
-                {/* <button onClick={() => fullpageApi.moveTo(1, 0)}>aaaa</button> */}
-              </div>
-              
-            </ReactFullpage.Wrapper>
+            <>
+            <div className="fixedHeader">
+              <Testeee onClick={() => fullpageApi.moveSectionDown()}> aaaaaa </Testeee>
+            </div>
+              <ReactFullpage.Wrapper>
+                <div className="section">
+                  <Hello />
+                </div>
+                <div className="section">
+                  <About />
+                </div>
+              </ReactFullpage.Wrapper>
+            </>
           );
         }}
       />
---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { HeaderWrapper } from "./styled";
+
+const Header = () => {
+  return (
+    <HeaderWrapper>
+      <a href="#sectiontest"> testeeeee </a>
+    </HeaderWrapper>
+  )
+};
+
+export default Header;
